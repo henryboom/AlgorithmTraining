fa70b14e64982df6098b0f36c84c68ebb252adcc
Henry <rstyly233@gmail.com>
2024-05-05 21:12:13 +0800

    24/05/05

--- AlTest/src/test/java/org/example/chongxinanpai.java
+++ AlTest/src/test/java/org/example/chongxinanpai.java
@@ -1,13 +1,17 @@
-
+package org.example;
 //leetcode submit region begin(Prohibit modification and deletion)
+
+import org.junit.Test;
+
+import java.util.*;
 
 /**
  * 这个类用于解决找到从给定起点到终点的最短路径的问题。
  * 它通过构建一个地图（Map）来存储所有的航班，并使用深度优先搜索（DFS）来遍历这些航班，
  * 从而找到一个有效的路径。
  */
-class Solution {
-    // 使用HashMap来存储航班，其中key是出发地，value是按优先级排序的目的地队列
+ public class chongxinanpai {
+    // 使用HashMap来存储航班，其中key是出发地，value是按优先级排序的目的地队列（小的在前）
     Map<String, PriorityQueue<String>> map = new HashMap<String, PriorityQueue<String>>();
     // 用来存储最终的行程
     List<String> itinerary = new LinkedList<String>();
@@ -18,6 +22,8 @@ class Solution {
      * @param tickets 机票列表，每个列表包含两个字符串，表示出发地和目的地。
      * @return 返回一个行程列表，表示从起点到终点的路径。
      */
+
+
     public List<String> findItinerary(List<List<String>> tickets) {
         // 遍历机票列表，构建航班地图
         for (List<String> ticket : tickets) {
@@ -36,6 +42,22 @@ class Solution {
         Collections.reverse(itinerary);
         return itinerary;
     }
+    @Test
+    public void testFindItinerary() {
+        // 构造测试用的机票列表
+        List<List<String>> tickets = new ArrayList<>();
+        tickets.add(Arrays.asList("JFK", "KUL"));
+        tickets.add(Arrays.asList("JFK", "NRT"));
+        tickets.add(Arrays.asList("NRT", "JFK"));
+
+        // 调用方法并获取返回结果
+        List<String> result = findItinerary(tickets);
+
+        // 打印结果或进行其他断言验证
+        System.out.println(result);
+    }
+
+
 
     /**
      * 使用深度优先搜索遍历航班地图。
@@ -50,6 +72,7 @@ class Solution {
         }
         // 将当前城市加入到行程列表中
         itinerary.add(curr);
+        System.out.println(curr);
     }
 }
 
