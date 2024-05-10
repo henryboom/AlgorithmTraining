a5562f3af683bacc5bdf5ad889b7c70509eec09b
Henry <rstyly233@gmail.com>
2024-05-10 20:05:27 +0800

    24/05/10

---
+++ AlgorithmJava/leetcode/editor/cn/[55]跳跃游戏.java
@@ -0,0 +1,17 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canJump(int[] nums) {
+        //设置一个每一次选择的最大范围cover
+        int cover =0;
+        if (nums.length==1)
+            return true;
+        for (int i = 0; i <=cover; i++) {//小于等于
+            cover=Math.max(nums[i]+i, cover);
+            if (cover>=nums.length-1)return true;//表示覆盖到了终点
+
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
