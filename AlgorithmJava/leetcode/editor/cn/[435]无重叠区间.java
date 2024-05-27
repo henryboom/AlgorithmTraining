8990db3c62627ef8d7cc0a5547fce1a7de995257
Henry <rstyly233@gmail.com>
2024-05-27 10:29:23 +0800

    24/05/27

--- AlgorithmJava/leetcode/editor/cn/[435]无重叠区间.java
+++ AlgorithmJava/leetcode/editor/cn/[435]无重叠区间.java
@@ -6,7 +6,16 @@ class Solution {
         Arrays.sort(intervals, (a,b)-> {
             return Integer.compare(a[0],b[0]);
         });
-
+        int count = 1;
+        for(int i = 1;i < intervals.length;i++){
+            if(intervals[i][0] < intervals[i-1][1]){
+                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
+                continue;
+            }else{
+                count++;
+            }
+        }
+        return intervals.length - count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
