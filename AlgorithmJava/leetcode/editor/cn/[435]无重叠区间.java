36b90af28ae3036a8e17ccb693c9eaa73b761eb7
Henry <rstyly233@gmail.com>
2024-05-28 20:03:17 +0800

    24/05/28

--- AlgorithmJava/leetcode/editor/cn/[435]无重叠区间.java
+++ AlgorithmJava/leetcode/editor/cn/[435]无重叠区间.java
@@ -3,19 +3,32 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         ///
+//        Arrays.sort(intervals, (a,b)-> {
+//            return Integer.compare(a[0],b[0]);
+//        });
+//        int count = 1;
+//        for(int i = 1;i < intervals.length;i++){
+//            if(intervals[i][0] < intervals[i-1][1]){
+//                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
+//                continue;
+//            }else{
+//                count++;
+//            }
+//        }
+//        return intervals.length - count;
         Arrays.sort(intervals, (a,b)-> {
             return Integer.compare(a[0],b[0]);
         });
-        int count = 1;
-        for(int i = 1;i < intervals.length;i++){
-            if(intervals[i][0] < intervals[i-1][1]){
-                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
-                continue;
-            }else{
-                count++;
+        int remove = 0;
+        int pre = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++) {
+            if(pre > intervals[i][0]) {
+                remove++;
+                pre = Math.min(pre, intervals[i][1]);
             }
+            else pre = intervals[i][1];
         }
-        return intervals.length - count;
+        return remove;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
