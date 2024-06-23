71a7a446fc8eb00541b17dc75b8e3eeebb18e949
Henry <rstyly233@gmail.com>
2024-06-23 15:41:11 +0800

    24/06/23

---
+++ AlgorithmJava/leetcode/editor/cn/[27]移除元素.java
@@ -0,0 +1,8 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
