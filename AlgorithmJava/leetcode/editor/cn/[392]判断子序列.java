b2664f2230c84b50eed0c9b3dbc29244e98538fb
HuangPei <rstyly233@gmail.com>
2024-08-03 14:35:24 +0800

    changes

---
+++ AlgorithmJava/leetcode/editor/cn/[392]判断子序列.java
@@ -0,0 +1,24 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int length1 = s.length(); int length2 = t.length();
+        int[][] dp = new int[length1+1][length2+1];
+        for(int i = 1; i <= length1; i++){
+            for(int j = 1; j <= length2; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    //因为下一次需要用到这一次的dp[i][j],所以将删除了j对应 的元素的前一个状态的数值
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        if(dp[length1][length2] == length1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
